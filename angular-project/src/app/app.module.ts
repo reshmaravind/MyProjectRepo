d1a8af7a5f6236ad997ab1e66501135a136eb0d4
reshmaravind <reshmaravin30@gmail.com>
2022-12-22 11:52:05 +0530

    first commit

---
+++ angular-project/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainScreenComponent } from './components/main-screen/main-screen.component';
+import { LoginComponent } from './components/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainScreenComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
